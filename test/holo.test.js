d5226902d7d2cf8c8da53e533e8fd1d2af068fdf
Bart Blast <bart@bartblast.com>
2026-03-24 00:18:26 +0100

    Add element opening tag rule

--- test/holo.test.js
+++ test/holo.test.js
@@ -3,11 +3,50 @@ const { tokenize } = require("./tokenization_helper");
 
 describe("HOLO grammar", () => {
   describe("plain text", () => {
-    it("tokenizes plain text with the top-level scope", async () => {
+    it("with the top-level scope", async () => {
       const tokens = await tokenize("hello world");
 
       const expected = [
         { text: "hello world", scopes: ["text.holo"] },
+      ];
+
+      assert.deepStrictEqual(tokens, expected);
+    });
+  });
+
+  describe("element opening tag", () => {
+    it("regular", async () => {
+      const tokens = await tokenize("<div>");
+
+      const expected = [
+        { text: "<", scopes: ["text.holo", "meta.tag.holo", "punctuation.definition.tag.begin.holo"] },
+        { text: "div", scopes: ["text.holo", "meta.tag.holo", "entity.name.tag.holo"] },
+        { text: ">", scopes: ["text.holo", "meta.tag.holo", "punctuation.definition.tag.end.holo"] },
+      ];
+
+      assert.deepStrictEqual(tokens, expected);
+    });
+
+    it("self-closing", async () => {
+      const tokens = await tokenize("<br />");
+
+      const expected = [
+        { text: "<", scopes: ["text.holo", "meta.tag.holo", "punctuation.definition.tag.begin.holo"] },
+        { text: "br", scopes: ["text.holo", "meta.tag.holo", "entity.name.tag.holo"] },
+        { text: " ", scopes: ["text.holo", "meta.tag.holo"] },
+        { text: "/>", scopes: ["text.holo", "meta.tag.holo", "punctuation.definition.tag.end.holo"] },
+      ];
+
+      assert.deepStrictEqual(tokens, expected);
+    });
+
+    it("custom element", async () => {
+      const tokens = await tokenize("<foo-bar>");
+
+      const expected = [
+        { text: "<", scopes: ["text.holo", "meta.tag.holo", "punctuation.definition.tag.begin.holo"] },
+        { text: "foo-bar", scopes: ["text.holo", "meta.tag.holo", "entity.name.tag.holo"] },
+        { text: ">", scopes: ["text.holo", "meta.tag.holo", "punctuation.definition.tag.end.holo"] },
       ];
 
       assert.deepStrictEqual(tokens, expected);
