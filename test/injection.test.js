f22fa1659e2b61bd170067cba317be180d34f0cf
Bart Blast <bart@bartblast.com>
2026-03-24 13:09:46 +0100

    Add injection grammar for ~HOLO inline

--- test/injection.test.js
+++ test/injection.test.js
@@ -90,4 +90,25 @@ describe("HOLO injection grammar", () => {
       assert.deepStrictEqual(tokens, expected);
     });
   });
+
+  describe("holo_sigil_inline", () => {
+    it("inline form with template content", async () => {
+      const tokens = await tokenize('~HOLO"<div>content</div>"');
+
+      const expected = [
+        { text: "~HOLO", scopes: ["source.elixir", "meta.embedded.holo.elixir", "keyword.operator.sigil.elixir"] },
+        { text: '"', scopes: ["source.elixir", "meta.embedded.holo.elixir", "punctuation.definition.string.begin.elixir"] },
+        { text: "<", scopes: ["source.elixir", "meta.embedded.holo.elixir", "text.holo", "meta.tag.holo", "punctuation.definition.tag.begin.holo"] },
+        { text: "div", scopes: ["source.elixir", "meta.embedded.holo.elixir", "text.holo", "meta.tag.holo", "entity.name.tag.holo"] },
+        { text: ">", scopes: ["source.elixir", "meta.embedded.holo.elixir", "text.holo", "meta.tag.holo", "punctuation.definition.tag.end.holo"] },
+        { text: "content", scopes: ["source.elixir", "meta.embedded.holo.elixir", "text.holo"] },
+        { text: "</", scopes: ["source.elixir", "meta.embedded.holo.elixir", "text.holo", "meta.tag.holo", "punctuation.definition.tag.begin.holo"] },
+        { text: "div", scopes: ["source.elixir", "meta.embedded.holo.elixir", "text.holo", "meta.tag.holo", "entity.name.tag.holo"] },
+        { text: ">", scopes: ["source.elixir", "meta.embedded.holo.elixir", "text.holo", "meta.tag.holo", "punctuation.definition.tag.end.holo"] },
+        { text: '"', scopes: ["source.elixir", "meta.embedded.holo.elixir", "punctuation.definition.string.end.elixir"] },
+      ];
+
+      assert.deepStrictEqual(tokens, expected);
+    });
+  });
 });
